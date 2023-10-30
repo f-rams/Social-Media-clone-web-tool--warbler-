7c06cc34b82e37a3809841796aac378864af1635
Felipe Ramos <ffrf88@icloud.com>
2023-10-30 14:23:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
@@ -9,20 +8,19 @@ from models import db, connect_db, User, Message, Likes
 
 CURR_USER_KEY = "curr_user"
 
-DATABASE_URL = 'postgresql:///warbler'
+DATABASE_URL = os.getenv('DATABASE_URL')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
 
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
-# 'postgresql:///warbler'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = SECRET_KEY
 # toolbar = DebugToolbarExtension(app)
 app.app_context().push()
 
