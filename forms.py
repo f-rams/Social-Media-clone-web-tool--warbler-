21923a13242563755e91acee5ebe73897efdae7d
Felipe Ramos <ffrf88@icloud.com>
2023-10-30 14:11:23 -0400

    Twitter Clone update

---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class EditForm(FlaskForm):
+
+    username = StringField('Username')
+    email = StringField('E-mail')
+    password = PasswordField('Password', validators=[Length(min=6)])
+    bio = StringField('Bio')
+    image_url = StringField('Image URL')
+    location = StringField('Location')
+    header_image_url = StringField('Header Image URL')
